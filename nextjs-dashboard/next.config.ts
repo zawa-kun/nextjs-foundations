621eead363ba88818f46161f0ab4ca27c9b2f022
zawa-kun <shult.0219@gmail.com>
2025-04-26 22:39:54 +0900

    10 部分的な事前レンダリング ppr

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: 'incremental' // ppr部分的な事前レンダリングー＞有効
+  }
 };
 
 export default nextConfig;
